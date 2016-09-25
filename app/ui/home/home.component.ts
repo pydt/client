aecc3845d7862a98d2710e85e4924928a83ced9e
Mike Rosack <mrosack@gmail.com>
2016-09-25 14:26:33 -0500

    check game status once a minute

--- app/ui/home/home.component.ts
+++ app/ui/home/home.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router }    from '@angular/router';
 
 import { ApiService } from '../shared/api.service';
 import { Config } from '../shared/config';
 import { ConfigService } from '../shared/config.service';
 import { ProfileCacheService } from '../shared/profileCache.service';
+import { Observable, Subscription } from 'rxjs';
 import * as _ from 'lodash';
 
 @Component({
   selector: 'home',
   templateUrl: './home/home.component.html'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   private games: any;
   private gamePlayerProfiles: any = {};
   private config: Config;
+  private timerSub: Subscription;
 
   constructor(private apiService: ApiService, private configService: ConfigService, private profileCache: ProfileCacheService, private router: Router) {}
 
@@ -23,17 +25,24 @@ export class HomeComponent implements OnInit {
       this.config = config;
     });
 
-    this.apiService.getUserGames().then(games => {
-      this.games = games;
+    const timer = Observable.timer(10, 60000);
+    this.timerSub = timer.subscribe(() => {
+      this.apiService.getUserGames().then(games => {
+        this.games = games;
 
-      let steamIds = _.uniq(_.flatMap(this.games, (game) => {
-        return _.map(game.players, 'steamId');
-      }));
+        let steamIds = _.uniq(_.flatMap(this.games, (game) => {
+          return _.map(game.players, 'steamId');
+        }));
 
-      return this.profileCache.getProfiles(steamIds as [string]).then(profiles => {
-        this.gamePlayerProfiles = profiles;
+        return this.profileCache.getProfiles(steamIds as [string]).then(profiles => {
+          this.gamePlayerProfiles = profiles;
+        });
       });
     });
+  }
+
+  ngOnDestroy() {
+    this.timerSub.unsubscribe();
   }
 
   playTurn(game) {
