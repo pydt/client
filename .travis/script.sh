4b5fc3a5ff6adf9fc6124b78b48732babbb3afcc
Mike Rosack <mrosack@gmail.com>
2020-05-23 09:33:46 -0500

    add new civs

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    npm run dist-mac
+    npm run dist-mac || travis_terminate 1
 else
     sudo apt-get install --no-install-recommends -y icnsutils graphicsmagick
-    npm run dist-linux
+    npm run dist-linux || travis_terminate 1
 fi
