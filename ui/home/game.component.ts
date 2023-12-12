7171ed28364356a922b2482c28ca1397699564bd
Mike Rosack <mrosack@gmail.com>
2023-12-12 14:02:16 -0600

    move last turn code to shared

--- ui/home/game.component.ts
+++ ui/home/game.component.ts
@@ -5,6 +5,7 @@ import { SafeMetadataLoader } from "../shared/safeMetadataLoader";
 import { PlayTurnState } from "../playTurn/playTurnState.service";
 import { DiscourseInfo } from "../shared/discourseInfo";
 import { RPC_TO_MAIN } from "../rpcChannels";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "pydt-game",
@@ -18,9 +19,9 @@ export class GameComponent implements OnInit, OnDestroy {
   @Input() yourTurn: boolean;
   @Input() discoursePostNumber: number;
   @Output() smackRead = new EventEmitter<number>();
-  private now: Date;
   updateDateHandle: NodeJS.Timeout;
   games: CivGame[] = [];
+  lastTurnText$: Observable<string>;
 
   constructor(
     private router: Router,
@@ -29,17 +30,13 @@ export class GameComponent implements OnInit, OnDestroy {
   ) {}
 
   async ngOnInit(): Promise<void> {
-    // Save current date to prevent "changed after it was checked" bugs
-    this.now = new Date();
-    this.updateDateHandle = setInterval(() => {
-      this.now = new Date();
-    }, 30 * 1000);
-
     const metadata = await this.metadataLoader.loadMetadata();
 
     if (metadata) {
       this.games = metadata.civGames;
     }
+
+    this.lastTurnText$ = CountdownUtility.lastTurnOrTimerExpires$(this.game);
   }
 
   ngOnDestroy(): void {
@@ -70,23 +67,5 @@ export class GameComponent implements OnInit, OnDestroy {
 
   get newDiscoursePost(): boolean {
     return DiscourseInfo.isNewSmackTalkPost(this.game, this.user, this.discoursePostNumber || 0);
-  }
-
-  get lastTurn(): string {
-    const lastTurnDate = this.game.lastTurnEndDate || this.game.updatedAt;
-
-    return CountdownUtility.countdownAgo(lastTurnDate, this.now);
-  }
-
-  get timerExpires(): string {
-    const lastTurnDate = this.game.lastTurnEndDate || this.game.updatedAt;
-    const expirationDate = new Date(lastTurnDate.getTime() + this.game.turnTimerMinutes * 60 * 1000);
-
-    if (expirationDate.getTime() - this.now.getTime() < 0) {
-      return "soon...";
-    }
-
-    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    return `in ${CountdownUtility.countdown(this.now, expirationDate)}`;
   }
 }
